2f72d535773626db44d5c7f791a6ee9f035d39cc
Manuel Jesús Barrera <mj.barrera8@gmail.com>
2026-03-23 16:55:19 +0000

    feat: index.js

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,8 +1,41 @@
 const core = require('@actions/core');
-
 async function run() {
-    // Escritura en la salida de la acción
-    core.info('I am a custom JS action');
+    const baseBranch = core.getInput('base-branch');
+    const targetBranch = core.getInput('target-branch');
+    const workingDirectory = core.getInput('working-directory');
+    const ghToken = core.getInput('gh-token');
+    const debug = core.getBooleanInput('debug');
+
+    core.info(`Debug mode: ${debug}`);
+    core.info(`Base branch: ${baseBranch}`);
+    core.info(`Target branch: ${targetBranch}`);
+    core.info(`Working directory: ${workingDirectory}`);
+
+    const exec = require('@actions/exec');
+    await exec.exec('npm', ['update'], { cwd: workingDirectory });
+
+    let gitStatusOutput = await exec.getExecOutput('git', ['status', '-s', 'package*.json'], { cwd: workingDirectory });
+    if (gitStatusOutput.stdout) {
+        core.info('There are updates available.');
+        const github = require('@actions/github');
+        const octokit = github.getOctokit(ghToken);
+        try {
+            await octokit.rest.pulls.create({
+                owner: github.context.repo.owner,
+                repo: github.context.repo.repo,
+                title: `Update NPM dependencies`,
+                body: `This pull request updates NPM packages`,
+                base: baseBranch,
+                head: targetBranch
+            });
+        } catch (e) {
+                core.error('[js-dependency-update] : Something went wrong while creating the PR. Check logs below.');
+                core.setFailed(e.message);
+                core.error(e);
+        }
+    } else {
+        core.info('There are no updates at this point in time.');
+    }
 }
-// Ejecuta la función run
+
 run();
