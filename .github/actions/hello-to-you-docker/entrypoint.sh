0013ce8fc66e3512f2fc1f074a6813c011fee223
Manuel Jesús Barrera <mj.barrera8@gmail.com>
2026-03-25 15:19:56 +0000

    feat: add docker

---
+++ .github/actions/hello-to-you-docker/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+   
+# Use INPUT_<INPUT_NAME> to get the value of an input
+echo "Hello, $INPUT_NAME"
+         
+# Write outputs to the $GITHUB_OUTPUT file
+echo "time=$(date)" >>"$GITHUB_OUTPUT"
